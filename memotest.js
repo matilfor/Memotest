686221b6d3e078713ab0b9c7db23706aa1737219
Matilde Fortunato <matilde84@gmail.com>
2018-11-16 21:25:03 -0300

    maquetado dinamico de imgs del tablero

--- memotest.js
+++ memotest.js
@@ -1,16 +1,16 @@
 const imagenes = [
-  {nombre: "img1", id: 'img/alce.jpg'},
-  {nombre: "img2", id: 'img/epelante.jpg'},
-  {nombre: "img3", id: 'img/nena.jpg'},
-  {nombre: "img4", id: 'img/peces.jpg'},
-  {nombre: "img5", id: 'img/unichancho.jpg'},
-  {nombre: "img6", id: 'img/zapas.jpg'},
-  {nombre: "img7", id: 'img/alce.jpg'},
-  {nombre: "img8", id: 'img/epelante.jpg'},
-  {nombre: "img9", id: 'img/nena.jpg'},
-  {nombre: "img10", id: 'img/peces.jpg'},
-  {nombre: "img11", id: 'img/unichancho.jpg'},
-  {nombre: "img12", id: 'img/zapas.jpg'},
+  {id: "img1", src: 'img/alce.jpg'},
+  {id: "img2", src: 'img/epelante.jpg'},
+  {id: "img3", src: 'img/nena.jpg'},
+  {id: "img4", src: 'img/peces.jpg'},
+  {id: "img5", src: 'img/unichancho.jpg'},
+  {id: "img6", src: 'img/zapas.jpg'},
+  {id: "img7", src: 'img/alce.jpg'},
+  {id: "img8", src: 'img/epelante.jpg'},
+  {id: "img9", src: 'img/nena.jpg'},
+  {id: "img10", src: 'img/peces.jpg'},
+  {id: "img11", src: 'img/unichancho.jpg'},
+  {id: "img12", src: 'img/zapas.jpg'},
 ]
 
 function shuffle(a) {
@@ -23,18 +23,17 @@ function shuffle(a) {
 
 shuffle(imagenes);
 
-var divsCard = $('.card');
-
-for (let i = 0; i < divsCard.length; i++) {
-    let divDestapada = $('<div></div>').addClass('card-destapada');
-    let imgDestapada = $('<img />').attr('src', imagenes[i]);
-    divDestapada.append(imgDestapada);
-    divsCard.eq(i).append(divDestapada);
+function crearDivsDestapada(){
+  for (let i = 0; i < imagenes.length; i++){
+    let divCard = $('<div></div>').addClass('card');
+    let divCardTapada = $('<div></div>').addClass('card-tapada');
+    let imgDivCardTapada = $('<img />').attr('src', 'img/tapada.jpg');
+    let divCardDestapada = $('<div></div>').addClass('card-destapada');
+    let imgDivCardDestapada = $('<img/>').attr('src', imagenes[i].src);
+    divCardDestapada.append(imgDivCardDestapada);
+    divCardTapada.append(imgDivCardTapada);
+    divCard.append(divCardTapada).append(divCardDestapada);
+    $('.tablero').append(divCard);
   }
-
-function validarNombre(){
-    var nombre = $("#inputNombre").val();
-    if (nombre === ""){
-        $('.error').show();
-    }
 }
+crearDivsDestapada();
